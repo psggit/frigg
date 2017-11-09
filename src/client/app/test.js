2ba535ca14c3c34224a70b27e9cd74055ce8ca49
Madhur Garg <madhur@hipbar.com>
2017-11-09 15:36:58 +0530

    updated config

--- src/client/app/test.js
+++
@@ -1,3 +0,0 @@
-const multiply = (a, b) => {
-  return a * b
-}
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const createWebpackConfig = require('./webpack.common.config');
 const env = process.env.NODE_ENV
 
-const scriptsBaseDir = path.resolve(__dirname, './src/client/app')
+const scriptsBaseDir = path.resolve(__dirname, './src/client/app/scripts')
 
 const entryPoints = {
-  test: path.join(scriptsBaseDir,  'test.js')
+  login: path.join(scriptsBaseDir,  'login.js')
 }
 module.exports = createWebpackConfig(env, entryPoints)
