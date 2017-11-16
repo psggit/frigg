ec49f3fc7dd93c94cabf8d74aadf0f980ac340eb
Madhur Garg <madhur@hipbar.com>
2017-11-16 19:33:54 +0530

    updated entries

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const env = process.env.NODE_ENV
 const scriptsBaseDir = path.resolve(__dirname, './src/client/app/scripts')
 
 const entryPoints = {
-  login: path.join(scriptsBaseDir,  'login.js')
+  login: path.join(scriptsBaseDir,  'login.js'),
+  home: path.join(scriptsBaseDir, 'home.js')
 }
 module.exports = createWebpackConfig(env, entryPoints)
