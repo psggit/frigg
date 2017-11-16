7d11b03435aa1586774b881bc3086f13af03ad6f
Madhur Garg <madhur@hipbar.com>
2017-11-16 19:48:07 +0530

    override scss

--- src/client/app/react-apps/home/components/header.js
+++ src/client/app/react-apps/home/components/header.js
@@ -4,10 +4,12 @@ import IconButton from 'material-ui/IconButton'
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu'
 import FlatButton from 'material-ui/FlatButton'
 import PropTypes from 'prop-types'
+import '@sass/components/_app-bar.scss'
 
 const Header = (props) => {
   return (
     <AppBar
+      className='app-bar'
       title="Super admin"
       onLeftIconButtonTouchTap={props.toggleDrawer}
       iconElementRight={<FlatButton label="Logout" />}
