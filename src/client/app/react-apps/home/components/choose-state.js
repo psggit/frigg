3d41e1e36d1354cecf42f565bbedbb1a6036a2ff
Madhur Garg <madhur@hipbar.com>
2017-11-23 16:31:57 +0530

    create new state

--- src/client/app/react-apps/home/components/choose-state.js
+++ src/client/app/react-apps/home/components/choose-state.js
@@ -2,40 +2,83 @@ import React from 'react'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
 import indiaStates from './../constants/india-states'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+
 
 class ChooseState extends React.Component {
   constructor() {
     super()
     this.state = {
-      value: 1,
-      city: indiaStates[0]
+      stateIdx: null,
+      isCreateNew: false
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.switchStateForms = this.switchStateForms.bind(this)
   }
-  handleChange(e, k, value) {
-    this.setState({ value })
-    // console.log(indiaStates[k].name);
+  handleChange(e, k) {
+    this.setState({ stateIdx: k + 1 })
     this.props.setStateData(indiaStates[k].name)
   }
+  handleInputChange(e, value) {
+    this.props.setStateData(value)
+  }
+  switchStateForms(value) {
+    this.props.setStateData(null)
+    this.setState({ isCreateNew: value, stateIdx: null })
+  }
   render() {
+    const { isCreateNew } = this.state
     return (
-      <SelectField
-        floatingLabelText="Choose state"
-        value={this.state.value}
-        onChange={this.handleChange}
-      >
+      <div>
         {
-          indiaStates.map((state, i) => {
-            return (
-              <MenuItem
-                value={i + 1}
-                key={`state-${i}`}
-                primaryText={state.name}
+          isCreateNew
+          ? <div>
+              {/* <h5 style={{marginBottom: '0', fontWeight: '500', fontSize: '14px', color: 'rgba(0, 0, 0, 0.87)'}}>Create new state</h5> */}
+              <TextField
+                defaultValue=""
+                floatingLabelText="Enter state name"
+                onChange={this.handleInputChange}
               />
-            )
-          })
+              <div>
+                <p style={{color: '#9b9b9b', margin: '20px 0'}}>or</p>
+                <RaisedButton
+                  label="Choose state"
+                  onClick={() => { this.switchStateForms(false) }}
+                  style={{marginRight: 12, marginTop: '10px'}}
+                />
+              </div>
+          </div>
+          : <div>
+              <SelectField
+                  floatingLabelText="Choose state"
+                  value={this.state.stateIdx}
+                  onChange={this.handleChange}
+                >
+                  {
+                    indiaStates.map((state, i) => {
+                      return (
+                        <MenuItem
+                          value={i + 1}
+                          key={`state-${i}`}
+                          primaryText={state.name}
+                        />
+                      )
+                    })
+                  }
+                </SelectField>
+                <p style={{color: '#9b9b9b', margin: '20px 0'}}>or</p>
+                <div>
+                  <RaisedButton
+                    label="Create new state"
+                    onClick={() => { this.switchStateForms(true) }}
+                    style={{marginRight: 12, marginTop: '10px'}}
+                  />
+                </div>
+            </div>
         }
-      </SelectField>
+      </div>
     )
   }
 }
