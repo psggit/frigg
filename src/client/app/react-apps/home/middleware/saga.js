9b9a2af9e7d9750e33df39643912034dd2547a19
Madhur Garg <madhur@hipbar.com>
2017-11-25 00:23:25 +0530

    fetch states, cities, geoboundary and fences

--- src/client/app/react-apps/home/middleware/saga.js
+++ src/client/app/react-apps/home/middleware/saga.js
@@ -1,16 +1,33 @@
 import { takeLatest, delay } from 'redux-saga'
 import { call, fork, put, race, take } from 'redux-saga/effects'
 import * as ActionTypes from './../constants/actions'
-// import * as utils from './utils'
+import * as Api from './api'
 
 /**
  * Handlers
  */
 function* fetchStates(action) {
-  console.log('ioehfiuwhoiu');
   try {
-    // const { data, meta } = yield call(utils.getData, action)
-    yield put({ type: ActionTypes.SUCCESS_FETCH_STATES })
+    const data = yield call(Api.fetchStates, action)
+    yield put({ type: ActionTypes.SUCCESS_FETCH_STATES, data })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+function* fetchCities(action) {
+  try {
+    const data = yield call(Api.fetchCities, action)
+    yield put({ type: ActionTypes.SUCCESS_FETCH_CITIES, data })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+function* fetchLocalities(action) {
+  try {
+    const data = yield call(Api.fetchLocalities, action)
+    yield put({ type: ActionTypes.SUCCESS_FETCH_LOCALITIES, data })
   } catch (err) {
     console.log(err)
   }
@@ -25,8 +42,22 @@ function* watchFetchStates() {
   }
 }
 
+function* watchFetchCities() {
+  while (true) {
+    yield* takeLatest(ActionTypes.REQUEST_FETCH_CITIES, fetchCities)
+  }
+}
+
+function* watchFetchLocalities() {
+  while (true) {
+    yield* takeLatest(ActionTypes.REQUEST_FETCH_LOCALITIES, fetchLocalities)
+  }
+}
+
 export default function* rootSaga() {
   yield [
-    fork(watchFetchStates)
+    fork(watchFetchStates),
+    fork(watchFetchCities),
+    fork(watchFetchLocalities)
   ]
 }
