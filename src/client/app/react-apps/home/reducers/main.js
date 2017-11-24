eb6b3ced39794d7776c30f7aad757633c779fd07
Madhur Garg <madhur@hipbar.com>
2017-11-25 00:22:45 +0530

    fetch states, cities, geoboundary and fences

--- src/client/app/react-apps/home/reducers/main.js
+++ src/client/app/react-apps/home/reducers/main.js
@@ -1,15 +1,35 @@
 import * as ActionTypes from './../constants/actions'
 
 const initialState = {
-  loadingStates: true
+  loadingStates: true,
+  loadingCities: true,
+  loadingLocalites: true,
+  statesData: [],
+  citiesData: [],
+  localities: []
 }
 
 const actionsMap = {
-  [ActionTypes.SUCCESS_FETCH_STATES]: (state, action) => (
-    Object.assign({}, state, {
-      loadingStates: false
+  [ActionTypes.SUCCESS_FETCH_STATES]: (state, action) => {
+    return Object.assign({}, state, {
+      loadingStates: false,
+      statesData: action.data.states
     })
-  )
+  },
+
+  [ActionTypes.SUCCESS_FETCH_CITIES]: (state, action) => {
+    return Object.assign({}, state, {
+      loadingCities: false,
+      citiesData: action.data.cities
+    })
+  },
+
+  [ActionTypes.SUCCESS_FETCH_LOCALITIES]: (state, action) => {
+    return Object.assign({}, state, {
+      loadingLocalites: false,
+      localities: action.data.fences
+    })
+  }
 }
 
 export default function reducer(state = initialState, action = {}) {
