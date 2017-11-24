d07848bf815d70966864b160fdbd0db13c096252
Madhur Garg <madhur@hipbar.com>
2017-11-24 23:37:41 +0530

    updated action creators

--- src/client/app/react-apps/home/actions/index.js
+++ src/client/app/react-apps/home/actions/index.js
@@ -3,3 +3,13 @@ import * as ActionTypes from './../constants/actions'
 export const fetchStates = () => ({
   type: ActionTypes.REQUEST_FETCH_STATES
 })
+
+export const fetchCities = data => ({
+  type: ActionTypes.REQUEST_FETCH_CITIES,
+  data
+})
+
+export const fetchLocalities = data => ({
+  type: ActionTypes.REQUEST_FETCH_LOCALITIES,
+  data
+})
