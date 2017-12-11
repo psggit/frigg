c550b9d40d83a0da21b3046819419cbfaf0d116c
Madhur Garg <madhur@hipbar.com>
2017-12-11 19:23:25 +0530

    set data using query params and fetchData onPopstate

--- src/client/app/react-apps/home/container/manage-cities.js
+++ src/client/app/react-apps/home/container/manage-cities.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
 import Snackbar from 'material-ui/Snackbar'
@@ -10,6 +11,10 @@ import CreateCity from './../components/manage-cities/create-city'
 import EditCity from './../components/manage-cities/edit-city'
 import ViewCities from './../components/manage-cities/view-cities'
 import RoleBasedComponent from '@components/RoleBasedComponent'
+import getIcon from './../components/icon-utils'
+import FilterModal from '@components/filter-modal'
+import { getQueryObj, getQueryUri } from '@utils/query-string-utils'
+// import '@sass/components/_button.scss'
 
 import * as Roles from './../constants/roles'
 
@@ -19,6 +24,7 @@ class ManageCities extends React.Component {
     this.state = {
       shouldMountEditState: false,
       shouldMountCreateState: false,
+      shouldMountFilterDialog: false,
       stateIdx: 0,
       snackBar: { open: false, message: '' }
     }
@@ -26,19 +32,39 @@ class ManageCities extends React.Component {
     this.unmountEditStateDialog = this.unmountEditStateDialog.bind(this)
     this.mountCreateStateDialog = this.mountCreateStateDialog.bind(this)
     this.unmountCreateStateDialog = this.unmountCreateStateDialog.bind(this)
+    this.mountFilterDialog = this.mountFilterDialog.bind(this)
+    this.unmountFilterModal = this.unmountFilterModal.bind(this)
     this.handleStateChange = this.handleStateChange.bind(this)
+    this.setQueryParamas = this.setQueryParamas.bind(this)
+    this.fetchData = this.fetchData.bind(this)
   }
 
   componentDidMount() {
+    this.fetchData()
+    window.onpopstate = this.fetchData
+  }
+
+  fetchData() {
     this.props.actions.setGeoboundaryLoadingState()
     this.props.actions.fetchStates()
-    // const queryString = this.props.location.search.slice(1)
-    // const queryParams = queryString.split('&')
-    // console.log(queryParams);
-    // this.setState({ stateIdx: parseInt(queryParams[0].split('=')[1]) })
-    // this.props.actions.fetchCities({
-    //   state_short_name: queryParams[1].split('=')[1]
-    // })
+    if (location.search.length) {
+      // if query string exists then apply filters
+      this.setQueryParamas()
+    } else {
+      // if there is no query string then fetch defult citiesData/all citiesData
+      // this.props.actions.fetchCities()
+    }
+  }
+
+  setQueryParamas() {
+    const queryUri = location.search.slice(1)
+    const queryObj = getQueryObj(queryUri)
+    Object.entries(queryObj).forEach((item) => {
+      this.setState({ [item[0]]: item[1] })
+    })
+    this.props.actions.fetchCities({
+      state_short_name: queryObj.short_name
+    })
   }
 
   setSnackBarOptions(options) {
@@ -60,18 +86,33 @@ class ManageCities extends React.Component {
     })
   }
 
+  unmountEditStateDialog() {
+    this.setState({ shouldMountEditState: false })
+  }
+
+  mountFilterDialog() {
+    this.setState({ shouldMountFilterDialog: true })
+  }
+
+  unmountFilterModal() {
+    this.setState({ shouldMountFilterDialog: false })
+  }
+
   handleStateChange(e, k) {
     const { statesData } = this.props
     const stateIdx = k + 1
     this.setState({ stateIdx })
-    // history.pushState(null, null, `/home/manage-cities?stateIdx=${stateIdx}&short_name=${statesData[k].short_name}`)
+
+    const queryObj = {
+      stateIdx,
+      short_name: statesData[k].short_name
+    }
+
+    history.pushState(queryObj, "city listing", `/home/manage-cities?${getQueryUri(queryObj)}`)
+
     this.props.actions.fetchCities({
       state_short_name: statesData[k].short_name
     })
-  }
-
-  unmountEditStateDialog() {
-    this.setState({ shouldMountEditState: false })
   }
 
   render() {
@@ -84,37 +125,29 @@ class ManageCities extends React.Component {
 
     return (
       <div style={{ width: '100%', maxWidth: 900, margin: 'auto' }}>
-        <RaisedButton
-          style={{ marginTop: '40px' }}
-          label="Create new city"
-          primary
-          onClick={this.mountCreateStateDialog}
-        />
+        <div
+          style={{
+            marginTop: '40px',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}
+        >
+          <RaisedButton
+            label="Create new city"
+            primary
+            onClick={this.mountCreateStateDialog}
+          />
 
-        <p style={{ color: '#9b9b9b', margin: '20px 0px 0 20px' }}>or</p>
+          <RaisedButton
+            onClick={this.mountFilterDialog}
+            label="Filter"
+            icon={getIcon('filter')}
+          />
+        </div>
+
+        {/* <p style={{ color: '#9b9b9b', margin: '20px 0px 0 20px' }}>or</p> */}
 
         <br />
-
-        <SelectField
-          floatingLabelText="Choose state"
-          value={this.state.stateIdx}
-          onChange={this.handleStateChange}
-        >
-          {
-            !loadingStates
-            ? (
-              statesData.map((state, i) => (
-                <MenuItem
-                  value={i + 1}
-                  key={state.id}
-                  primaryText={state.state_name}
-                />
-              ))
-            )
-            : ''
-          }
-        </SelectField>
-
 
         <ViewCities
           citiesData={citiesData}
@@ -141,6 +174,43 @@ class ManageCities extends React.Component {
               shouldMountCreateState={this.state.shouldMountCreateState}
               unmountCreateStateDialog={this.unmountCreateStateDialog}
             />
+          )
+          : ''
+        }
+
+        {
+          this.state.shouldMountFilterDialog
+          ? (
+            <FilterModal
+              title="Filter Cities"
+              unmountFilterModal={this.unmountFilterModal}
+            >
+              <div style={{
+                display: 'flex'
+              }}
+              >
+                <SelectField
+                  floatingLabelText="Choose state"
+                  value={parseInt(this.state.stateIdx)}
+                  onChange={this.handleStateChange}
+                >
+                  {
+                    !loadingStates
+                    ? (
+                      statesData.map((state, i) => (
+                        <MenuItem
+                          value={i + 1}
+                          key={state.id}
+                          primaryText={state.state_name}
+                        />
+                      ))
+                    )
+                    : ''
+                  }
+                </SelectField>
+
+              </div>
+            </FilterModal>
           )
           : ''
         }
