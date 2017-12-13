5511270e87ece04e7ae1b9a18590ce65469458ef
Madhur Garg <madhur@hipbar.com>
2017-12-13 17:39:08 +0530

    using ifElse component

--- src/client/app/react-apps/home/components/manage-cities/view-city-details.js
+++ src/client/app/react-apps/home/components/manage-cities/view-city-details.js
@@ -1,58 +1,48 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import * as Actions from './../../actions'
-import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 import { NavLink } from 'react-router-dom'
-import Checkbox from 'material-ui/Checkbox'
 import { getQueryObj } from '@utils/url-utils'
 import '@sass/components/_form.scss'
+import CityDetailsForm from './city-details-form'
+import IfElse from '@components/declarative-if-else'
 
 class ViewCity extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      isCityActive: null,
-      cityName: null,
       isEdit: false
     }
-    this.handleTextFields = this.handleTextFields.bind(this)
-    this.handleCheckboxes = this.handleCheckboxes.bind(this)
+
     this.enableEditMode = this.enableEditMode.bind(this)
     this.disableEditMode = this.disableEditMode.bind(this)
     this.update = this.update.bind(this)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (Object.keys(nextProps.geoBoundaryData).length) {
-      this.setState({
-        isCityActive: nextProps.geoBoundaryData.is_available,
-        cityName: nextProps.geoBoundaryData.name
-      })
-    }
   }
 
   componentDidMount() {
     const { actions, match } = this.props
     const queryObj = getQueryObj(location.search.slice(1))
     this.setState({ queryObj })
-    actions.viewGeoboundary({
+    actions.fetchCityDetails({
       id: parseInt(queryObj.id)
     })
   }
 
   update() {
-    const { geoBoundaryData } = this.props
+    const { cityDetails } = this.props
     const { isCityActive, cityName, queryObj } = this.state
+    const data = this.cityDetailsForm.getData()
+
     this.props.actions.updateCity({
       id: parseInt(queryObj.id),
-      is_available: isCityActive,
-      deliverable_city: geoBoundaryData.deliverable_city,
-      state_short_name: geoBoundaryData.state_short_name,
-      gps: geoBoundaryData.gps,
-      name: cityName,
-      geoboundary: geoBoundaryData.geoboundary
+      is_available: data.isCityActive,
+      deliverable_city: cityDetails.deliverable_city,
+      state_short_name: cityDetails.state_short_name,
+      gps: cityDetails.gps,
+      name: data.cityName,
+      geoboundary: cityDetails.geoboundary
     }, this.disableEditMode)
   }
 
@@ -64,16 +54,15 @@ class ViewCity extends React.Component {
     this.setState({ isEdit: false })
   }
 
-  handleCheckboxes(e) {
-    this.setState({ [e.target.name]: e.target.checked })
-  }
-
-  handleTextFields(e) {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
   render() {
-    const { loadingGeoboundary, geoBoundaryData } = this.props
+    const { loadingCityDetails, cityDetails } = this.props
+
+    if (!loadingCityDetails) {
+      let urlArray = location.pathname.split("/")
+      urlArray[3] = cityDetails.name
+      let url = urlArray.join('/')
+      history.pushState(null, null, `${url}?id=${cityDetails.id}`)
+    }
     return (
       <div style={{
         position: 'relative',
@@ -84,122 +73,96 @@ class ViewCity extends React.Component {
       }}
       >
         {
-          !loadingGeoboundary
+          !loadingCityDetails
           ? (
             <div style={{ position: 'absolute', right: '0' }}>
-              {
-                !this.state.isEdit
-                ? (
+
+              <IfElse conditionMet={!this.state.isEdit}>
+                <RaisedButton
+                  primary
+                  label="Edit City"
+                  onClick={this.enableEditMode}
+                  style={{ marginRight: '20px' }}
+                />
+                <RaisedButton
+                  primary
+                  label="Cancel"
+                  onClick={this.disableEditMode}
+                  style={{ marginRight: '20px' }}
+                />
+              </IfElse>
+
+              <IfElse conditionMet={cityDetails.geoboundary.length || cityDetails.geoboundary}>
+                <NavLink
+                  target="_blank"
+                  exact
+                  to={`${location.pathname}/boundary?id=${this.state.queryObj.id}`}
+                  href={`${location.pathname}/boundary?id=${this.state.queryObj.id}`}
+                >
                   <RaisedButton
                     primary
-                    label="Edit City"
-                    onClick={this.enableEditMode}
+                    label="View city boundary"
                     style={{ marginRight: '20px' }}
                   />
-                )
-                : (
+                </NavLink>
+
+                <NavLink
+                  target="_blank"
+                  exact
+                  to={`${location.pathname}/create-boundary?id=${this.state.queryObj.id}`}
+                  href={`${location.pathname}/create-boundary?id=${this.state.queryObj.id}`}
+                >
                   <RaisedButton
                     primary
-                    label="Cancel"
-                    onClick={this.disableEditMode}
-                    style={{ marginRight: '20px' }}
+                    label="Create new"
                   />
-                )
-              }
-              {
-                geoBoundaryData.geoboundary.length || geoBoundaryData.geoboundary
-                ? (
-                  <NavLink
-                    target="_blank"
-                    exact
-                    to={`${location.pathname}/boundary?id=${this.state.queryObj.id}`}
-                    href={`${location.pathname}/boundary?id=${this.state.queryObj.id}`}
-                  >
-                    <RaisedButton
-                      primary
-                      label="View city boundary"
-                      style={{ marginRight: '20px' }}
-                    />
-                  </NavLink>
-                )
-                : (
-                  <NavLink
-                    target="_blank"
-                    exact
-                    to={`${location.pathname}/create-boundary?id=${this.state.queryObj.id}`}
-                    href={`${location.pathname}/create-boundary?id=${this.state.queryObj.id}`}
-                  >
-                    <RaisedButton
-                      primary
-                      label="Create new"
-                    />
-                  </NavLink>
-                )
-              }
-              {
-                geoBoundaryData.geoboundary.length || geoBoundaryData.geoboundary
-                ? (
-                  <NavLink
-                    target="_blank"
-                    exact
-                    to={`${location.pathname}/localities?id=${this.state.queryObj.id}`}
-                    href={`${location.pathname}/localities?id=${this.state.queryObj.id}`}
-                  >
-                    <RaisedButton
-                      primary
-                      label="View localities"
-                    />
-                  </NavLink>
-                )
-                : ''
-              }
+                </NavLink>
+              </IfElse>
+
+              <IfElse conditionMet={cityDetails.geoboundary.length || cityDetails.geoboundary}>
+                <NavLink
+                  target="_blank"
+                  exact
+                  to={`${location.pathname}/localities?id=${this.state.queryObj.id}`}
+                  href={`${location.pathname}/localities?id=${this.state.queryObj.id}`}
+                >
+                  <RaisedButton
+                    primary
+                    label="View localities"
+                  />
+                </NavLink>
+                <Fragment />
+              </IfElse>
+
             </div>
           )
           : ''
         }
 
-        {
-          !loadingGeoboundary
-          ? (
-            <div style={{ paddingTop: '40px' }}>
+        <IfElse conditionMet={!loadingCityDetails}>
+          <div style={{ paddingTop: '40px' }}>
 
-              <div className="form-group">
-                <p className="label">Name</p>
-                <TextField
-                  disabled={!this.state.isEdit}
-                  onChange={this.handleTextFields}
-                  name="cityName"
-                  value={this.state.cityName}
-                />
-              </div>
+            <CityDetailsForm
+              ref={(node) => { this.cityDetailsForm = node }}
+              isDisabled={!this.state.isEdit}
+              isCityActive={cityDetails.is_available}
+              cityName={cityDetails.name}
+            />
 
-              <div className="form-group">
-                <p className="label">Active</p>
-                <Checkbox
-                  disabled={!this.state.isEdit}
-                  checked={this.state.isCityActive}
-                  onCheck={this.handleCheckboxes}
-                  name="isCityActive"
-                  label="is_active"
-                />
-              </div>
+            <IfElse conditionMet={this.state.isEdit}>
+              <RaisedButton
+                primary
+                label="Update changes"
+                onClick={this.update}
+                style={{ marginTop: '40px' }}
+              />
+              <Fragment />
+            </IfElse>
 
-              {
-                this.state.isEdit
-                ? (
-                  <RaisedButton
-                    primary
-                    label="Update changes"
-                    onClick={this.update}
-                    style={{ marginTop: '40px' }}
-                  />
-                )
-                : ''
-              }
-            </div>
-          )
-          : 'loading..'
-        }
+          </div>
+          <div>loading..</div>
+        </IfElse>
+
       </div>
     )
   }
