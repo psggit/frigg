76550ccec2b9e3422bdbf8074b2c3ebda53593a1
Madhur Garg <madhur@hipbar.com>
2017-12-19 17:17:43 +0530

    menu item override css

--- src/client/app/react-apps/home/components/navigation-bar.js
+++ src/client/app/react-apps/home/components/navigation-bar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Drawer from 'material-ui/Drawer'
 import { NavLink } from 'react-router-dom'
 import MenuItem from 'material-ui/MenuItem'
+import '@sass/components/_menu-item.scss'
+import  { getIcon } from '@utils/icons-utils'
 
 class NavigationBar extends  React.Component {
   constructor() {
@@ -18,15 +20,19 @@ class NavigationBar extends  React.Component {
     ]
     return (
       <Drawer
+        className="drawer"
         docked={false}
         label="Default"
         open={this.props.isDrawerOpen}
         onRequestChange={this.props.toggleDrawer}
       >
+        <div>
+          <MenuItem className="menu-item-heading">Super admin-v2</MenuItem>
+        </div>
         {
           navigationItems.map((item, i) => (
             <a href={item.path} key={`nav-item-${i}`}>
-              <MenuItem onClick={() => { this.handleClick(item.name) }}>{item.name}</MenuItem>
+              <MenuItem className="menu-item" onClick={() => { this.handleClick(item.name) }}>{item.name}</MenuItem>
             </a>
           ))
         }
