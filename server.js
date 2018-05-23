96da97127fd41429be8a4b2c258d4e1f8d4e1837
Madhur Garg <madhur@hipbar.com>
2018-05-23 13:26:03 +0530

    remoevd gzip compression

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express()
 const env = process.env.NODE_ENV
 
 if (env === 'production') {
-  app.get('*.css', (req, res, next) => {
-    console.log(req.url);
-    req.url += '.gz'
-    res.set('Content-Encoding', 'gzip')
-    next()
-  })
+  // app.get('*.css', (req, res, next) => {
+  //   console.log(req.url);
+  //   req.url += '.gz'
+  //   res.set('Content-Encoding', 'gzip')
+  //   next()
+  // })
 
   app.get('*.js', (req, res, next) => {
     console.log(req.url);
