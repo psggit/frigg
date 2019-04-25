577602fdc79e20598b5b68de0d0dc89b2eaecb8a
Aparna Chinnaiah <aparna.chinnaiah@hipbar.com>
2019-04-25 17:31:35 +0530

    Added update company file

---
+++ src/react-apps/home/components/manage-company/update-company.js
@@ -0,0 +1,93 @@
+import React from "react"
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as Actions from '../../actions/index'
+import CompanyForm from "./company-form"
+import {formatStateAndCityList} from "@utils/response-format-utils"
+
+class UpdateCompany extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      stateList: [],
+      cityList: [],
+      stateMap: {},
+      updatingCompany: false
+    }
+    this.handleSave = this.handleSave.bind(this)
+    this.successStateCallback = this.successStateCallback.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.actions.fetchStateList({}, this.successStateCallback)
+  }
+
+  successStateCallback() {
+    // let cityList = [], stateMap = {}
+    // const stateList = this.props.stateList.map((item, i) => {
+    //   stateMap[item.state_id] = item.cities.map((city, index) => {
+    //     cityList[index] =  {
+    //       text: city.city_name,
+    //       value: city.city_id
+    //     }
+    //     return {
+    //       text: city.city_name,
+    //       value: city.city_id
+    //     }
+    //   })
+      
+    //   return {
+    //     text: item.state_name,
+    //     value: item.state_id
+    //   }
+    // })
+    // // console.log("state list", stateList, cityList, stateMap)
+    // this.setState({stateList, cityList, stateMap})
+    const {stateList, cityList, stateMap} = formatStateAndCityList(this.props.stateList)
+    this.setState({stateList, cityList, stateMap})
+  }
+
+  handleSave() {
+    const companyForm = this.companyForm.getData()
+    console.log("form data", this.companyForm)
+    this.setState({updatingCompany: true})
+    this.props.actions.updateCompany({
+      id: parseInt(this.props.location.state.id),
+      name: companyForm.companyName,
+      city_id: parseInt(companyForm.selectedCityId),
+      state_id: parseInt(companyForm.selectedStateId),
+      address: companyForm.address,
+      pin_code: parseInt(companyForm.pincode)
+    }, () => {
+      this.setState({updatingCompany: false})
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h4 style={{ margin: '0', marginBottom: '40px' }}>Enter company details</h4>
+        <CompanyForm 
+          ref={(node) => { this.companyForm = node }}
+          stateList={this.state.stateList}
+          cityList={this.state.cityList}
+          stateMap={this.state.stateMap}
+          data={this.props.location.state}
+          updatingCompany={this.state.updatingCompany}
+          handleSave={this.handleSave}
+        />
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = state => state.main
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(Actions, dispatch)
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UpdateCompany)
