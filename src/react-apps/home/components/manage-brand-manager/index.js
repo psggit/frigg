cd0dabedf2d745621b92cf2d002a6a5107880dae
Madhur Garg <madhur@hipbar.com>
2019-06-03 15:59:09 +0530

    passing hasuara id for updating brand manager

--- src/react-apps/home/components/manage-brand-manager/index.js
+++ src/react-apps/home/components/manage-brand-manager/index.js
@@ -9,20 +9,20 @@ import {
 } from 'material-ui/Table'
 import TableLoadingShell from '../table-loading-shell'
 import '@sass/components/_table.scss'
-import {overrideTableStyle} from './../../../utils'
+import { overrideTableStyle } from './../../../utils'
 import Moment from "moment"
 
 const TableHeaderItems = [
   '',
   'ID',
   'NAME',
-	'EMAIL',
-	'MOBILE',
-	'COMPANY',
-	'STATUS',
-	'KYC STATUS',
-	'CREATED AT',
-	'UPDATED AT'
+  'EMAIL',
+  'MOBILE',
+  'COMPANY',
+  'STATUS',
+  'KYC STATUS',
+  'CREATED AT',
+  'UPDATED AT'
 ]
 
 const styles = [
@@ -30,12 +30,12 @@ const styles = [
   { width: '38px' },
   { width: '120px' },
   { width: '120px' },
-	{ width: '100px' },
-	{ width: '100px' },
-	{ width: '100px' },
-	{ width: '100px' },
-	{ width: '100px' },
-	{ width: '100px' }
+  { width: '100px' },
+  { width: '100px' },
+  { width: '100px' },
+  { width: '100px' },
+  { width: '100px' },
+  { width: '100px' }
 ]
 
 class ViewBrandManagerList extends React.Component {
@@ -44,26 +44,26 @@ class ViewBrandManagerList extends React.Component {
     super()
 
     this.editBrandManagerDetails = this.editBrandManagerDetails.bind(this)
-	}
-	
-	componentDidMount() {
-		this.overrideTableStyle()
-	}
+  }
 
-	overrideTableStyle() {
+  componentDidMount() {
+    this.overrideTableStyle()
+  }
+
+  overrideTableStyle() {
     // document.querySelectorAll(".bordered--table")[1].parentElement.style.overflow = "auto"
     overrideTableStyle()
   }
 
   editBrandManagerDetails(e, item) {
     e.stopPropagation()
-    this.props.history.push(`/home/manage-brand-manager/edit/${item.id}`, item)
+    this.props.history.push(`/home/manage-brand-manager/edit/${item.id}?hid=${item.hasura_id}`, item)
   }
 
   render() {
     const {
-			loadingBrandManagers,
-			brandManagers
+      loadingBrandManagers,
+      brandManagers
     } = this.props
     return (
       <div>
@@ -101,12 +101,12 @@ class ViewBrandManagerList extends React.Component {
                         <TableRowColumn style={styles[2]}>{item.name}</TableRowColumn>
                         <TableRowColumn style={styles[3]}>{item.email}</TableRowColumn>
                         <TableRowColumn style={styles[4]}>{item.mobile_number}</TableRowColumn>
-												<TableRowColumn style={styles[5]}>{item.company_name}</TableRowColumn>
-												<TableRowColumn style={styles[6]}>{item.activity_status ? 'Active' : 'Inactive'}</TableRowColumn>
-												<TableRowColumn style={styles[7]}>{item.kyc_status}</TableRowColumn>
-												<TableRowColumn style={styles[8]}>{Moment(item.created_at).format("DD-MM-YYYY h:mm:s")}</TableRowColumn>
-												<TableRowColumn style={styles[9]}>{Moment(item.updated_at).format("DD-MM-YYYY h:mm:s")}</TableRowColumn>
-                      </TableRow> 
+                        <TableRowColumn style={styles[5]}>{item.company_name}</TableRowColumn>
+                        <TableRowColumn style={styles[6]}>{item.activity_status ? 'Active' : 'Inactive'}</TableRowColumn>
+                        <TableRowColumn style={styles[7]}>{item.kyc_status}</TableRowColumn>
+                        <TableRowColumn style={styles[8]}>{Moment(item.created_at).format("DD-MM-YYYY h:mm:s")}</TableRowColumn>
+                        <TableRowColumn style={styles[9]}>{Moment(item.updated_at).format("DD-MM-YYYY h:mm:s")}</TableRowColumn>
+                      </TableRow>
                     )
                   })
                 )
