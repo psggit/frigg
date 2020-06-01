932674b3f378cb02d7bb17cb3510cda0a1ac87bc
NishankParameswaran <nishant.parameswaran@hipbar.com>
2020-06-01 18:12:25 +0530

    Removed city dropdown and changed reatailer and warehouse to text-fields

--- src/react-apps/home/components/manage-retailer-warehouse-mapping/map-retailer-to-warehouse.js
+++ src/react-apps/home/components/manage-retailer-warehouse-mapping/map-retailer-to-warehouse.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Card } from 'material-ui/Card'
 import * as Api from "./../../middleware/api"
-import SelectField from 'material-ui/SelectField'
-import MenuItem from 'material-ui/MenuItem'
+// import SelectField from 'material-ui/SelectField'
+import TextField from 'material-ui/TextField'
+// import MenuItem from 'material-ui/MenuItem'
 import RaisedButton from 'material-ui/RaisedButton'
 
 class MapRetailerToWarehouse extends React.Component {
@@ -20,96 +21,101 @@ class MapRetailerToWarehouse extends React.Component {
       warehouseList: []
     }
 
-    this.handleRetailerChange = this.handleRetailerChange.bind(this) 
-    this.handleWarehouseChange = this.handleWarehouseChange.bind(this)
-    this.handleCityChange = this.handleCityChange.bind(this) 
-    this.fetchRetailerList = this.fetchRetailerList.bind(this)
-    this.fetchWarehouseList = this.fetchWarehouseList.bind(this)
+    // this.handleRetailerChange = this.handleRetailerChange.bind(this) 
+    // this.handleWarehouseChange = this.handleWarehouseChange.bind(this)
+    // this.handleCityChange = this.handleCityChange.bind(this) 
+    this.handleTextFields = this.handleTextFields.bind(this)
+    // this.fetchRetailerList = this.fetchRetailerList.bind(this)
+    // this.fetchWarehouseList = this.fetchWarehouseList.bind(this)
     this.handleSave = this.handleSave.bind(this)
   }
 
-  componentDidMount () {
-    this.fetchCities()
+  // componentDidMount () {
+  //   this.fetchCities()
+  // }
+
+  // fetchCities () {
+  //   Api.fetchCities({
+  //     data: {
+  //       state_short_name: null,
+  //       is_available: false,
+  //       offset: 0,
+  //       limit: 1000,
+  //       deliverable_city: true,
+  //       no_filter: true
+  //     }
+  //   })
+  //   .then((response) => {
+  //     this.setState({
+  //       cityList: response.cities,
+  //       selectedCityIdx: response.cities[0].id
+  //     })
+  //     this.fetchRetailerList()
+  //     this.fetchWarehouseList()
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error in fetching cities list", error)
+  //   })
+  // }
+
+  // handleRetailerChange (e, k) {
+  //   const selectedRetailerIdx = this.state.retailerList[k].id
+  //   this.setState({ selectedRetailerIdx })
+  // }
+
+  // handleWarehouseChange (e, k) {
+  //   const selectedWarehouseIdx = this.state.warehouseList[k].id
+  //   this.setState({ selectedWarehouseIdx })
+  // }
+
+  // handleCityChange (e, k) {
+  //   const selectedCityIdx = this.state.cityList[k].id
+  //   this.setState({ selectedCityIdx }, () => {
+  //     this.fetchRetailerList()
+  //     this.fetchWarehouseList()
+  //   })
+  // }
+
+  handleTextFields (e) {
+    this.setState({ [e.target.name]: e.target.value })
   }
 
-  fetchCities () {
-    Api.fetchCities({
-      data: {
-        state_short_name: null,
-        is_available: false,
-        offset: 0,
-        limit: 1000,
-        deliverable_city: true,
-        no_filter: true
-      }
-    })
-    .then((response) => {
-      this.setState({
-        cityList: response.cities,
-        selectedCityIdx: response.cities[0].id
-      })
-      this.fetchRetailerList()
-      this.fetchWarehouseList()
-    })
-    .catch((error) => {
-      console.log("Error in fetching cities list", error)
-    })
-  }
+  // fetchRetailerList () {
+  //   Api.fetchRetailers({
+  //     data: {
+  //       city_id: parseInt(this.state.selectedCityIdx),
+  //       is_available: false,
+  //       offset: 0,
+  //       limit: 1000,
+  //       no_filter: false
+  //     }
+  //   })
+  //   .then((response) => {
+  //     this.setState({
+  //       retailerList: response.retailer_data,
+  //       selectedRetailerIdx: response.retailer_data[0].id
+  //     })
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error in fetching retailer list", error)
+  //   })
+  // }
 
-  handleRetailerChange (e, k) {
-    const selectedRetailerIdx = this.state.retailerList[k].id
-    this.setState({ selectedRetailerIdx })
-  }
-
-  handleWarehouseChange (e, k) {
-    const selectedWarehouseIdx = this.state.warehouseList[k].id
-    this.setState({ selectedWarehouseIdx })
-  }
-
-  handleCityChange (e, k) {
-    const selectedCityIdx = this.state.cityList[k].id
-    this.setState({ selectedCityIdx }, () => {
-      this.fetchRetailerList()
-      this.fetchWarehouseList()
-    })
-  }
-
-  fetchRetailerList () {
-    Api.fetchRetailers({
-      data: {
-        city_id: parseInt(this.state.selectedCityIdx),
-        is_available: false,
-        offset: 0,
-        limit: 1000,
-        no_filter: false
-      }
-    })
-    .then((response) => {
-      this.setState({
-        retailerList: response.retailer_data,
-        selectedRetailerIdx: response.retailer_data[0].id
-      })
-    })
-    .catch((error) => {
-      console.log("Error in fetching retailer list", error)
-    })
-  }
-
-  fetchWarehouseList () {
-    Api.fetchWarehouses({
-      pagination: { "limit": 1000, "offset": 0 },
-      filter: { "field": "city_id", "value": this.state.selectedCityIdx.toString() }
-    })
-    .then((response) => {
-      this.setState({
-        warehouseList: response.message,
-        selectedWarehouseIdx: response.message[0].id
-      })
-    })
-    .catch((error) => {
-      console.log("Error in fetching warehouse list", error)
-    })
-  }
+  // fetchWarehouseList () {
+  //   Api.fetchWarehouses({
+  //     pagination: { "limit": 1000, "offset": 0 },
+  //     filter: { "field": "city_id", "value": this.state.selectedCityIdx.toString() }
+  //   })
+  //   .then((response) => {
+  //     this.setState({
+  //       warehouseList: response.message,
+  //       selectedWarehouseIdx: response.message[0].id
+  //     })
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error in fetching warehouse list", error)
+  //   })
+  // }
 
   handleSave () {
     this.setState({ mappingRetailerToWarehouse: true})
@@ -139,7 +145,7 @@ class MapRetailerToWarehouse extends React.Component {
       }}
       >
         <h4 style={{ margin: '0', marginBottom: '40px' }}>Map retailer to warehouse</h4>
-        <div className="form-group">
+        {/* <div className="form-group">
           <label className="label">City</label><br />
           <SelectField
             value={this.state.selectedCityIdx}
@@ -155,14 +161,17 @@ class MapRetailerToWarehouse extends React.Component {
               ))
             }
           </SelectField>
-        </div>
+        </div> */}
         <div className="form-group">
           <label className="label">Retailer</label><br />
-          <SelectField
-            value={this.state.selectedRetailerIdx}
-            onChange={this.handleRetailerChange}
-          >
-            {
+          <TextField
+            disabled={false}
+            name="selectedRetailerIdx"
+            defaultValue={this.state.selectedRetailerIdx}
+            style={{ width: '100%' }}
+            onChange={this.handleTextFields}
+          />
+            {/* {
               this.state.retailerList.map((item, i) => (
                 <MenuItem
                   value={item.id}
@@ -171,11 +180,18 @@ class MapRetailerToWarehouse extends React.Component {
                 />
               ))
             }
-          </SelectField>
+          </SelectField> */}
         </div>
         <div className="form-group">
           <label className="label">Warehouse</label><br />
-          <SelectField
+          <TextField
+            disabled={false}
+            name="selectedWarehouseIdx"
+            defaultValue={this.state.selectedWarehouseIdx}
+            style={{ width: '100%' }}
+            onChange={this.handleTextFields}
+          />
+          {/* <SelectField
             value={this.state.selectedWarehouseIdx}
             onChange={this.handleWarehouseChange}
           >
@@ -188,7 +204,7 @@ class MapRetailerToWarehouse extends React.Component {
                 />
               ))
             }
-          </SelectField>
+          </SelectField> */}
         </div>
         <div className="form-group">
           <RaisedButton
